db42331fe8921c781c3001775f930d00d59ac10c
diepan-mindx <diepau1312@mindx.net.vn>
2025-09-14 11:27:57 +0700

    b1

---
+++ B1/ontap.py
@@ -0,0 +1,24 @@
+# nguoi dung nhap tu console so nguyen duong n 
+# in ra tam giac vuong voi n dong + so luong * = so thu tu dong
+
+#vd: n = 3
+#*
+#**
+#***
+
+while True:
+    n = input("Nhap so nguyen duong n: ")
+    if n == "exit":
+        print("Tam biet!")
+        break
+    try:
+        n = int(n)
+        if n <= 0:
+            print("Vui long nhap so nguyen duong!")
+            continue
+    except Exception as e:
+        print("Loi: ", e)
+        continue
+        
+    for i in range (n):
+        print("*" * (i + 1))
